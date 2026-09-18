c02f0c4128934b6f6bca4fce6c68372786b970d7
JustSpalton <michael.spalton@gmail.com>
2026-09-18 21:40:25 +0100

    Connect Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// Filled with the new project's web-app settings before deployment.
 window.DESIGN_HOURS_FIREBASE_CONFIG = {
-  apiKey: "REPLACE_AFTER_PROJECT_CREATION",
-  authDomain: "REPLACE_AFTER_PROJECT_CREATION",
-  projectId: "REPLACE_AFTER_PROJECT_CREATION",
-  storageBucket: "REPLACE_AFTER_PROJECT_CREATION",
-  messagingSenderId: "REPLACE_AFTER_PROJECT_CREATION",
-  appId: "REPLACE_AFTER_PROJECT_CREATION"
+  apiKey: "AIzaSyAdJcjBriuLL9B27PmrrG8yjviDzZ9C0F4",
+  authDomain: "design-hours-tracker.firebaseapp.com",
+  projectId: "design-hours-tracker",
+  storageBucket: "design-hours-tracker.firebasestorage.app",
+  messagingSenderId: "648673925892",
+  appId: "1:648673925892:web:a293f266265499ec214e51"
 };
